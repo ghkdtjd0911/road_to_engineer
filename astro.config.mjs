797e1a800a67562ef0ed540b2f0618cf90619c37
Sung Hwang <82424873+ghkdtjd0911@users.noreply.github.com>
2024-06-23 18:05:56 +0900

    추가설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ghkdtjd0911.github.io/my-road_to_engineer',
-	// base: '',
+	site: 'https://ghkdtjd0911.github.io/',
+	base: '/road_to_engineer',
 	integrations: [
 		starlight({
 			title: '개발자 몸부림 페이지',
